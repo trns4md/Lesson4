76b74ea3fefec8fc5e480bd0750c65d2a912b273
Becky White <trns4md@charter.net>
2017-07-19 12:48:58 -0400

    tables

---
+++ Multiplication/Multiplytable.js
@@ -0,0 +1,10 @@
+for(var i =1; i<=12; i++){
+    console.log("The " + i + "'s Table");
+
+for (var j =0; j< 13; j++){
+    console.log( i +"x" + j);
+}
+if (i<12){
+    console.log("___________");
+}
+}
