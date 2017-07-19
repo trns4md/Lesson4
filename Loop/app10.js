c79c4eeec73a3bb173f33c722fb1ee13fd255ae5
Becky White <trns4md@charter.net>
2017-07-19 12:14:36 -0400

    Loop

--- Loop/app10.js
+++ Loop/app10.js
@@ -1,4 +1,7 @@
-for (var i =0; i<10; i++)
-    {
-    console.log("Hello, World!");
+var whoTogreet = "Eva";
+var N = 12
+for(var i =0; i < N; i++)
+if(i % 2 !=0)
+{
+        console.log(i +" Hello " + whoTogreet + "!");
     }
